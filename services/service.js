8d7c631399b0e2620bf7dde162e7d69a759f880a
morrismago <morrismago@hotmail.com>
2016-06-13 23:26:44 -0500

    Sitio de noticias utilizando angular y bootstrap

---
+++ services/service.js
@@ -0,0 +1,11 @@
+﻿angular.module('AppNewsService', ['ngResource']). //servicio para obtener la lista de noticias
+
+    factory('NewsFactoty', function ($resource) {
+
+        return $resource(':resourceName.json', {}, {
+
+            query: { method: 'GET', params: { resourceName: 'json/news_mock' }, isArray: true }
+
+        });
+
+    });
